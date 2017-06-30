9055d75e732f99d209a31dcf4c111415bae564cf
Leonardo Medeiros <leohsmedeiros@gmail.com>
2017-06-30 19:35:23 -0300

    Scripts with Events, HandsRepository and GameControllers

---
+++ RockPaperScissor_Unity/Assets/Scripts/Content/Inherit/HandOptions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandOptions {
+
+	public string Id { private set; get; }
+	public Sprite Image { private set; get; }
+	public List<HandOptions> Win { private set; get; }
+
+	public HandOptions (string id, Sprite image)
+	{
+		Id = id;
+		Image = image;
+		Win = new List<HandOptions> ();
+	}
+
+	public void SetWinList (List<HandOptions> win)
+	{
+		Win = win;
+	}
+
+	public bool CheckIfCanBeat (HandOptions handOption)
+	{
+		return (Win.Find (x => x.Id.Equals (handOption.Id)) != null);
+	}
+}
---
+++ RockPaperScissor_Unity/Assets/Scripts/Frontend/ButtonEvents.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvents : MonoBehaviour {
+
+	public GameController GameCtrl;
+
+	public void OnButtonPlayerVsCPU ()
+	{
+		GameCtrl.ChangeGameMode (GameController.GameModes.Player_CPU, GameCtrl.PlayerNameInput.text);
+		GameCtrl.ScreenChanger.ShowScreen (ChangeScreen.ScreenName.ScreenGame);
+	}
+
+	public void OnButtonCPUVsCPU ()
+	{
+		GameCtrl.ChangeGameMode (GameController.GameModes.CPU_CPU, "CPU");
+		GameCtrl.ScreenChanger.ShowScreen (ChangeScreen.ScreenName.ScreenGame);
+	}
+
+	public void OnButtonReturn ()
+	{
+		GameCtrl.ScreenChanger.ShowScreen (ChangeScreen.ScreenName.ScreenGameModeMenu);
+	}
+
+	public void OnButtonNextMatch ()
+	{
+		GameCtrl.NextMatch ();
+	}
+
+	public void OnButtonChooseHandOption (string HandId)
+	{
+		GameCtrl.SetPlayerChoice (HandId);
+	}
+
+}
